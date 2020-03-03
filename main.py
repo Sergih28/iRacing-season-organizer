42fd557e078828808175d94c5f000e298d969ece
Sergi Heras <sergihese@hotmail.com>
2020-03-03 15:26:19 +0100

    reading pdf and handling errors if not found

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import pprint
 import sys
 
 
-def get_PDF_file():
+def main():
+    pdf_filename = get_PDF_filename()
+    try:
+        pdf_file = open(pdf_filename, 'rb')  # creating pdf object
+    except:
+        sys.exit('Can\'t find the specified file')
+    pdf_fileReader = PyPDF2.PdfFileReader(pdf_file)  # creating pdf reader obj
+    # print(pdf_fileReader.getPage(43).extractText())
+
+
+def get_PDF_filename():
     if len(sys.argv) > 1:
         return sys.argv[1]
     return sys.exit('You need to specify the pdf location as the first argument!')
 
 
-get_PDF_file()
-
-# print(get_PDF_file())
-
-# creating an object
-# file = open('2020S2.pdf', 'rb')
-
-# creating a pdf reader object
-# fileReader = PyPDF2.PdfFileReader(file)
+main()
 
 # print the number of pages in pdf file
 # print(fileReader.getPage(4))
